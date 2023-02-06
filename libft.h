7cda21295be934b733d6123e2383c757eef4cc09
Alexander <83783125+SAEZITO@users.noreply.github.com>
2023-02-06 12:21:30 +0100

    Add files via upload

--- libft.h
+++ libft.h
@@ -37,7 +37,14 @@ char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 
 int		ft_atoi(const char *str);
+char    *ft_itoa(int n);
 char	*ft_strchr(const char *s, int c);
+void    ft_striteri(const char *s, void (*f)(unsigned int, char));
+
+void    ft_putchar_fd(char c, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -45,5 +52,10 @@ size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
+char    *ft_strmapi(const char *s, char (*f)(unsigned int, char));
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	**ft_split(char const *s, char c);
 
 #endif
